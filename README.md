66baf374df0476266d4380c444506d5ab4e8fb09
Sean CAI <seancai78@gmail.com>
2024-06-18 23:32:41 +0800

    完善readme以及完成等號的尋找

---
+++ README.md
@@ -0,0 +1,22 @@
+# 一、這是一個在Zerojudge上的題目
+## 1.題目為解碼一組等式，找到各項符號的意義
+## 2.網址為：https://zerojudge.tw/ShowProblem?problemid=a082
+# 二、目前思路
+## 1.先找必為數字的位置，求出符號可能的位置
+## 2.數字必在符號、頭尾及重複的情況
+## .只有13個符號，因此目前使用int做位元運算
+<br/>
+
+# 三、目前程式作用
+## 1.目前已可找出等號的位置
+## 2.變數意義
+```cpp
+    const int pow13 = pow(13)-1;//節省多次使用的計算時間
+    int is_num=0;//是數字的位置
+    int is_sign=0;//是符號的位置(is_num ^ pow13)
+    int equal_sign= pow13;//等號的位置(位元運算用)
+    int equal_pos;//等號的位置
+    int n;//等式數量
+    string equation[n];//輸入等式的儲存區
+    int bit_equation[n];//紀錄出現過的符號
+```
--- a082.cpp
+++ a082.cpp
@@ -2,6 +2,7 @@
 #include<map>
 #include<iostream>
 #include <bitset>
+#include<string.h>
 
 using namespace std;
 
@@ -16,22 +17,23 @@ int main()
 {
     const int pow13 = pow(13)-1;
     int is_num=0;
-    int is_equal=0;
-    int equal_sign;
-    equal_sign = pow13;
+    int is_sign=0;
+    int equal_sign= pow13;
     int equal_pos;
     int n;
     scanf("%d",&n);
-    char equation[n][11];
+    string equation[n];
     int bit_equation[n];
     for (int i = 0; i < n; i++)
     {
-        scanf("%s",equation[i]);
-        int len = sizeof(equation)/sizeof(char);
+        cin>>equation[i];
+        int len = equation[i].size();
+        printf("len: %d\n",len);
         bit_equation[i]=0;
         //is num => aa is num; abcde ae is num
         is_num |=pow(equation[i][0]-'a');//head
         is_num |=pow(equation[i][len-1]-'a');//end
+        //printf("equation[%d]= %c",i,equation[i][0]);
         bit_equation[i] |= pow(equation[i][0]-'a');
         for (int j = 1; j < len; j++)
         {
@@ -40,16 +42,19 @@ int main()
                 is_num |=pow(equation[i][j]-'a');
             }
             bit_equation[i] |= pow(equation[i][j]-'a');
+            //printf("%c",equation[i][j]);
         }
-        is_equal = is_num ^pow13;
+        //printf("\n");
+        is_sign = is_num ^pow13;
         equal_sign &= bit_equation[i];
-        cout<<bitset<13>(bit_equation[i])<<endl;
-        equal_sign &= is_num;
-        cout<<bitset<13>(equal_sign)<<endl;
+        equal_sign &= is_sign;
+        cout<<"equal_sign= "<<bitset<13>(equal_sign)<<endl;
+        cout<<"is_num= "<<bitset<13>(is_num)<<endl;
+        cout<<"is_sign= "<<bitset<13>(is_sign)<<endl;
         if(__builtin_popcount(equal_sign) == 1)
         {
             equal_pos=__builtin_ctz(equal_sign);
-            printf("equal position: %d\n",&equal_pos);
+            printf("equal position: %d\n",equal_pos);
         }
     }
     
